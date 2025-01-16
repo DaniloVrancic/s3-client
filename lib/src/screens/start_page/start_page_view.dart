8577c01be3944d3c3b1f3120f170cd2a8d9b5d71
DaniloVrancic <danilov98@hotmail.com>
2025-01-16 15:09:04 +0100

    DW 0.1.3: Started working on Start page and linking up assets

--- lib/src/screens/start_page/start_page_view.dart
+++ lib/src/screens/start_page/start_page_view.dart
@@ -7,8 +7,42 @@ class StartPageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        alignment: Alignment.center,
-        child: const Text('Hello World'),
+        color: Colors.black,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Opacity(
+                opacity: 0.4,
+                child: Image.asset('assets/images/welcome_page_bg.png', 
+              fit: BoxFit.cover),
+              )
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ClipOval(
+                    child: Container(
+                      width: 180,
+                      height: 180,
+                      color: const Color.fromARGB(255, 79, 8, 133),
+                      alignment: Alignment.center,
+                      child: Image.asset('assets/images/logo_with_name_no_background_preview.png',
+                      fit: BoxFit.cover,
+                      alignment: Alignment.center),
+
+                    ),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    child: const Text("Welcome to Bucketer, your trusted S3 storage manager!"),
+                  )
+                ],  
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
