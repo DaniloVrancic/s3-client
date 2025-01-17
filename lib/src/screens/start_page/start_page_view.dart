50069b978eb870b7ce7f72069d861faa92f61cc0
DaniloVrancic <danilov98@hotmail.com>
2025-01-17 15:22:17 +0100

    DW 0.1.4: Working on Starter page

--- lib/src/screens/start_page/start_page_view.dart
+++ lib/src/screens/start_page/start_page_view.dart
@@ -7,38 +7,74 @@ class StartPageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.black,
+        color: Colors.white60,
         child: Stack(
           children: [
             Positioned.fill(
               child: Opacity(
                 opacity: 0.4,
                 child: Image.asset('assets/images/welcome_page_bg.png', 
-              fit: BoxFit.cover),
+              fit: BoxFit.fill),
               )
             ),
             Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  ClipOval(
+                  Center(
+                    child: ClipOval(
                     child: Container(
                       width: 180,
                       height: 180,
-                      color: const Color.fromARGB(255, 79, 8, 133),
+                      padding: const EdgeInsets.all(15),
+                      color: const Color.fromARGB(255, 174, 109, 223),
                       alignment: Alignment.center,
                       child: Image.asset('assets/images/logo_with_name_no_background_preview.png',
-                      fit: BoxFit.cover,
-                      alignment: Alignment.center),
-
+                        fit: BoxFit.scaleDown,
+                        alignment: Alignment.center),
+                        
+                        ),
+                      ),
+                  ),
+                  
+                      const SizedBox(height: 50,),
+                  
+                  Container(
+                    child: const Text("Bucketer",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w700
+                    ),
                     ),
                   ),
+                  const SizedBox(height: 30,),
                   Container(
-                    color: Colors.white,
-                    child: const Text("Welcome to Bucketer, your trusted S3 storage manager!"),
-                  )
-                ],  
+                    color: const Color.fromARGB(180, 0, 0, 0),
+                    child : const Text("Your trusted S3 storage manager!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white70,
+                      fontWeight: FontWeight.w600
+                    ),
+                    ),
+                  ),
+                 const SizedBox(height: 30,),
+                 ElevatedButton(
+                  onPressed: (){}, 
+                  style: ButtonStyle(
+                    shape: WidgetStateProperty<OutlinedBorder>(
+                      OutlinedBorder
+                    ),
+                  ),
+                  child: const Text("Login")),
+                 ElevatedButton(
+                  onPressed: (){}, 
+                  child: const Text("Register")),
+                ],
               ),
             )
           ],
